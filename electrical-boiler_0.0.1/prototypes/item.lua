9c3a455bfba2f04bbf30a9dc33220596ff08309e
noeve <ejb@u.northwestern.edu>
2016-07-23 13:03:10 -0700

    Revert "Item.lua creation"
    This reverts commit df5c2edd38e710c9f1b796091ed7ff348295665b.

--- electrical-boiler_0.0.1/prototypes/item.lua
+++ electrical-boiler_0.0.1/prototypes/item.lua
@@ -1,18 +1,1 @@
---[[ Copyright (c) noeve & silentlord
- * Part of the electrical boiler mod
- *
- * See LICENCE in the project directory for licence information
---]]
 
-data:extend({
-
-	{
-		type = "item",
-		name = "eboiler",
-		icon = "__electrical-boiler__/graphics/icons/foobaricon.png",
-		flags = {"goes-to-quickbar"},
-		order = "a-b-c",
-		stack_size = 50,
-		place_result = "eboiler",
-	}
-})
